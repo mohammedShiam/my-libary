b87f9e7b7a5cbb9d8a15e6fd1badab03067fccba
Mohammed <official.shiam123@gail.com>
2021-09-22 10:11:14 +0600

    Finished Project

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,11 +1,26 @@
+const rootStyle = window.getComputedStyle(document.documentElement)
 
-FilePond.registerPlugin(
+if (rootStyle.getPropertyValue('--book-cover-width-large') != null && rootStyle.getPropertyValue('--book-cover-width-large') !== '') {
+  ready();
+}else{
+  document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready() {
+
+  const coverWidth = parseFloat(rootStyle.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyle.getPropertyValue('--book-cover-aspect-ratio'))
+  const coverHeight = coverWidth / coverAspectRatio
+  FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
   )
 
   FilePond.setOptions({
-    stylePanelAspectRatio:150/100
+    stylePanelAspectRatio:1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight
   })
-FilePond.parse(document.body)
+  FilePond.parse(document.body)
+}
